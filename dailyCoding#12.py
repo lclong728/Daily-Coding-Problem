5ef72a142c6d2d17db487e8566d78d5387f20cc3
lclong728 <lclong728@gmail.com>
2018-08-04 22:58:30 +0800

    problem 12: adding dynamic programming method

--- dailyCoding#12.py
+++ dailyCoding#12.py
@@ -34,17 +34,22 @@ print(step_counter(8))
 print("--------------------")
 
 def step_counter_2(N,step_list):
-    return counter2_helper(N, step_list)
+    memo = dict()
+    return counter2_helper(N, step_list, memo)
 
-def counter2_helper(N, step_list):
+def counter2_helper(N, step_list, memory): 
     if N == 0 or N == 1:
         return 1
     else:
-        result = 0
-        for step in step_list:
-            if N >= step:
-                result += counter2_helper(N-step, step_list)
-        return result
+        if N in memory:
+            return memory[N]
+        else:
+            result = 0
+            for step in step_list:
+                if N >= step:
+                    result += counter2_helper(N-step, step_list, memory)
+                    memory[N] = result
+            return result
     
 print("ans for solution 2 @climb any step in step_list:")
 print(step_counter_2(8, [1,3,5])) 
